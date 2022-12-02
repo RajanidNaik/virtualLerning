365675a4b72f023a69752d6719138b38948a182b
deekshi-c <deekshith.c@robosoftin.com>
2022-12-02 10:30:17 +0530

    virtualLearn

--- virtualLerning/src/app/material/material.module.ts
+++ virtualLerning/src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import { MatIconModule } from '@angular/material/icon';
 
 const materialComponent=[]
 
 @NgModule({
-  exports: [MatInputModule],
-  imports: [MatInputModule],
+  exports: [MatInputModule, MatProgressBarModule, MatIconModule],
+  imports: [MatInputModule, MatProgressBarModule, MatIconModule],
 })
 export class MaterialModule {}
