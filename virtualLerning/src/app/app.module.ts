3888880fc39949d1ff2f058893e7b63c334c3cf4
deekshi-c <deekshith.c@robosoftin.com>
2022-12-06 18:55:03 +0530

    virtualLearn

--- virtualLerning/src/app/app.module.ts
+++ virtualLerning/src/app/app.module.ts
@@ -1,32 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-
 import { SecondHeadComponent } from './second-head/second-head.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from './material/material.module';
 import { SideNavComponent } from './side-nav/side-nav.component';
-
-
-
 import { DashBoardComponent } from './dash-board/dash-board.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { SettingsComponent } from './settings/settings.component';
-
 import { DialogDashboardComponent } from './dialog-dashboard/dialog-dashboard.component';
 import { ProfileDialogComponent } from './profile-dialog/profile-dialog.component';
 import { HeadDialogComponent } from './head-dialog/head-dialog.component';
-
 import { OtpComponent } from './otp/otp.component';
 import { PasswordChangeComponent } from './password-change/password-change.component';
 import { VideoComponent } from './video/video.component';
 import { CertificateComponent } from './certificate/certificate.component';
 import { QAndAComponent } from './q-and-a/q-and-a.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -53,12 +48,9 @@ import { QAndAComponent } from './q-and-a/q-and-a.component';
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
-
+    ReactiveFormsModule,
     BrowserAnimationsModule,
-    
-    
-    
-
+    FormsModule,
     BrowserAnimationsModule
 
   ],
--- virtualLerning/src/app/otp/otp.component.ts
+++ virtualLerning/src/app/otp/otp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-otp',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./otp.component.css']
 })
 export class OtpComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private router:Router) { }
+  val1:any
+  val2:any;
+  val3:any;
+  val4:any;
+  otp:any
   ngOnInit(): void {
   }
-resend(){}
+   
+resend(){
+  console.log(typeof this.val1);
+  
 }
+jump(e:any ,first:any,mid:any,last:any){
+ var length = mid.value.length;
+ var maxlength = mid.getAttribute('maxlength');
+ if(length == maxlength){
+  if( last !=" "){
+  last.focus();
+  }
+ } 
+ if(e.key === "Backspace"){
+  first.focus();
+ }
+}
+checkNum(event:any){
+  const char= (event.which)?event.which: event.keycode;
+  if(char > 31 && (char < 48 || char >57)) return false;
+  else return true;
+}
+verify(){
+  this.otp = this.val1+this.val2+this.val3+this.val4;
+  console.log("OTP"+this.otp);
+  this.router.navigateByUrl('/password');
+}
+
+
+}
+
+
