9d1b6556a8026d24ba631ae324ac414394decc17
deekshi-c <deekshith.c@robosoftin.com>
2022-12-14 13:12:46 +0530

    virtu

--- virtualLerning/src/app/services/admin-login.service.ts
+++ virtualLerning/src/app/services/admin-login.service.ts
@@ -8,17 +8,41 @@ export class AdminLoginService {
   constructor(private http: HttpClient) {}
   login =
     'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin';
+
   adminLogin(body: any) {
     return this.http.put(
       'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin/login',
       body,
-      { responseType: 'text' }
+      { observe: 'response' }
     );
   }
   resetPass(body: any) {
     return this.http.post(
       'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin/send',
-      body,{ observe:"response"}
+      body,
+      { observe: 'response' }
+    );
+  }
+  otp(body: any) {
+    return this.http.post(
+      'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin/verify',
+      body,
+      { observe: 'response' }
+    );
+  }
+  resend(body: any) {
+    return this.http.post(
+      'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin/resend',
+      body,
+      { observe: 'response' }
+    );
+  }
+
+  reset(body: any) {
+    return this.http.put(
+      'http://virtuallearnadmin-env.eba-vvpawj4n.ap-south-1.elasticbeanstalk.com/admin/resetPassword',
+      body,
+      { observe: 'response' }
     );
   }
 }
